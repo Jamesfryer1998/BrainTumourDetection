a6460ff47f322a0fe1f7bf6c67a723090439d1f0
james fryer <jamesfryer1998@outlook.com>
2023-01-11 12:03:14 +0000

    ImagePreProcessing Structure

--- pre_processing.py
+++ pre_processing.py
@@ -1,2 +1,23 @@
 import datetime
 import pandas as pd
+
+class ImagePreProcessing:
+    def __init__(self, image):
+        self.image = image
+        self.cleaned_image = None
+
+    def resizing(self):
+        return
+
+    def squishing(self):
+        return
+
+    def cropping(self):
+        return
+
+    def pro_process_data(self):
+        self.resizing()
+        self.squishing()
+        self.cropping()
+
+        return self.cleaned_image
